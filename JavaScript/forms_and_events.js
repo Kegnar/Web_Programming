f5c9751129da91816ff37b391b6f482fe05fc58d
Kegnar <mitriy.exe@gmail.com>
2026-07-24 07:25:40 +0400

    in process

---
+++ JavaScript/forms_and_events.js
@@ -0,0 +1,59 @@
+﻿// JavaScript source code
+function Factorial()
+{
+    let numberElement = document.getElementById("number");
+    let number = numberElement.value;
+    let resultElement = document.getElementById("factorial-result");
+    let f = BigInt(1);
+    for (let i = 1n; i <= number; i++)
+    {
+        f *= i;
+    }
+    //resultElement.value = `${number}! = ${f}`;
+    resultElement.innerHTML = `${number}! = ${f}`;
+}
+
+function setImage()
+{
+    let filename = document.getElementById("image-file");
+    let reader = new FileReader();
+    reader.onload = function (e)
+    {
+        document.getElementById("image").src = e.target.result;
+    }
+    reader.readAsDataURL(filename.files[0]);
+}
+function setBackgroundColor(event)
+{
+    document.body.style.backgroundColor = event.target.value;
+    console.log(event.target.id);
+    //document.body.style.backgroundColor = document.getElementById("background-color").value;
+    /*let color = document.getElementById("background-color").value;
+    document.body.style.backgroundColor = color;
+    alert("setBackgroundColor");*/
+}
+function setForegroundColor()
+{
+    document.body.style.color = document.getElementById("foreground-color").value;
+}
+function setColor(event)
+{
+    /*
+    --------------------------
+    ==  - ñðàâíèâàåò äâà çíà÷åíèÿ;
+    === - ñðàâíèâàåò äâà çíà÷åíèÿ è òèïû ýòèõ çíà÷åíèé;
+    === - âîçâðàùàåò 'true' òîëüêî â òîì ñëó÷àå,
+            åñëè ñîâïàäàþò êàê çíà÷åíèÿ, òàê è òèïû;
+    --------------------------
+     */
+    document.body.style[(event.target.id === 'background-color' ? 'backgroundColor' : 'color')] = event.target.value;
+    //event.target.id === 'background-color' ? document.body.style.backgroundColor : document.body.style.color = event.target.value;
+    /*
+    if (event.target.id === 'background-color')
+        document.body.style.backgroundColor = event.target.value;
+    else
+        document.body.style.color = event.target.value;
+    */
+    console.log(event.target.id);
+    //alert("setColor");
+}
