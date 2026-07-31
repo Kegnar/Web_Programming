36ab09c0dd3ff80e6a1a797d5072f3ab96476766
Kegnar <mitriy.exe@gmail.com>
2026-07-31 19:47:46 +0400

    clock done

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -134,3 +134,41 @@ function tickTimer()
     setTimeout(tickTimer, 100);
 }
 tickTimer();
+
+
+//--------------------------
+document.getElementById('btn-start').addEventListener('click', startCountdownTimer);
+function startCountdownTimer() {
+    let targetDate = document.getElementById("target-date");
+    let targetTime = document.getElementById("target-time");
+    let btnStart = document.getElementById("btn-start");
+    if(btnStart.value === "Start")
+    {
+        btnStart.value = "Stop";
+        targetDate.disabled = targetTime.disabled = true;
+        tickCountdown();
+    }
+    else
+    {
+        btnStart.value = "Start";
+        targetDate.disabled = targetTime.disabled = false;
+    }
+}
+function tickCountdown()
+{
+    let now = new Date();
+
+    let targetDateControl = document.getElementById("target-date");
+    let targetTimeControl = document.getElementById("target-time");
+
+    let targetDateValue = targetDateControl.valueAsDate;
+    let targetTimeValue = targetTimeControl.valueAsDate;
+
+    document.getElementById("target-date-value").innerHTML = targetDateValue;
+    document.getElementById("target-time-value").innerHTML = targetTimeValue;
+    //Выравниваем часовой пояс
+    document.getElementById("timezone").innerHTML = now.getTimezoneOffset()/60;
+    targetDateValue.setHours(targetDateValue.getHours() + targetDateValue.getTimezoneOffset()/60);
+    // targetTimeValue.setHours(targetTimeValue.getHours() + targetTimeValue.getTimezoneOffset()/60);
+
+}
